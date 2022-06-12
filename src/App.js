5ba6a78760908b6794cedd4871446c330ad7491f
cmjoshimul <cmjoshimul@gmail.com>
2022-06-12 08:24:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi There <code> I got changed</code>
         </p>
         <a
           className="App-link"
